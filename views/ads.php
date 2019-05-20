e828b7ab82ad6e6c068779844b9e4c290ac3c414
Peterson Macedo <petersondmb@hotmail.com>
2019-05-20 16:17:20 -0300

    retirada de alguns blocos da página ads

--- views/ads.php
+++ views/ads.php
@@ -7,7 +7,7 @@
 	/////////////// BLOCO 3 CIÊNCIA COMPROVADA
 	////////////////////////////////////// -->
 	<section id="ciencia">
-		<div class="first">
+		<div class="first" style="margin-bottom: 0;">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-6 right">
@@ -31,74 +31,6 @@
 			<div class="divisor hidden-sm hidden-xs"></div>
 		</div>
 
-		<div class="container last">
-			<div class="row items_1">
-				<div class="col-md-5">							
-					<figure>
-						<img src="assets/img/complete-justification.jpg" alt="">
-					</figure>
-					<div>	    						
-						<h3>Completa</h3>
-    					<p>Nenhuma outra fórmula contém ingredientes 100% naturais, seguros e eficazes para ajudar estabelecer o equilíbrio metabólico e ativar o seu metabolismo no modo queimador de gordura. Este fator é crucial para acelerar a perda de peso. Afinal, com o metabolismo operando em ritmo lento nada irá contribuir para que você emagreça de forma eficiente.</p>
-					</div>
-				</div>
-
-				<div class="col-md-2 hidden-sm hidden-xs" style="display: flex; align-items: center;justify-content: center;">
-					<i style="height: 150px; width: 1px; background: #ddd;"></i>
-				</div>
-				
-				<div class="col-md-5">							
-					<figure>
-						<img src="assets/img/proven-justification.jpg" alt="">
-					</figure>
-					<div>	    						
-						<h3>Comprovada</h3>
-    					<p>Diversos estudos realizados nos EUA revelam que os compostos do Phenatrim – ácido clorogênico, sinefrina, caffeine anhydrous, irvingia gabonensis, metilxantinas e Green tea – intensificam consideravelmente a oxidação de gordura, especialmente aquela que se localiza na barriga, nas coxas e no bumbum.</p>
-					</div>
-				</div>
-			</div>
-
-			<div class="row items-divisor"> <div class="col-12 hidden-xs hidden-sm"><hr></div> </div>
-
-			<div class="row items_1">
-				<div class="col-md-5">							
-					<figure>
-						<img src="assets/img/powerful-justification.jpg" alt="">
-					</figure>
-					<div>
-						<h3>Potente</h3>
-						<p>Uma mistura única que reúne as melhores substâncias aprovadas pela ANVISA, na dosagem exata para ser capaz de proporcionar os melhores resultados, sem risco e sem efeito sanfona.</p>
-					</div>
-				</div>
-
-				<div class="col-md-2 hidden-sm hidden-xs" style="display: flex; align-items: center;justify-content: center;">
-					<i style="height: 150px; width: 1px; background: #ddd;"></i>
-				</div>
-
-				<div class="col-md-5">							
-					<figure>
-						<img src="assets/img/synergy-justification.jpg" alt="">
-					</figure>
-					<div>
-						<h3>Sinérgica</h3>
-    					<p>Cada componente foi cuidadosamente selecionado e trabalha em perfeita sinergia, atacando a gordura diretamente em sua fonte, promovendo assim até 3x mais perda de peso.</p>
-    				</div>
-				</div>
-			</div>
-
-			<div class="row" style="margin-top: 50px; margin-bottom: 50px;" align="center">
-				<a href="https://phenatrim.com.br/#order" id="cta-button" class="img-responsive"> 
-					<div>
-						<span>EXPERIMENTE PHENATRIM HOJE!</span>
-						<small>SEJA A PRÓXIMA HISTÓRIA DE SUCESSO</small>
-					</div>
-					<div>
-						<i class="fa fa-angle-double-right"></i>
-					</div>
-				</a>
-			</div>
-
-		</div>
 	</section>
 
 
@@ -206,39 +138,6 @@
 				</div>	
 			</section>
 
-			
-			<section id="autoridade-medica">
-				<div class="container">
-					<div class="row">							
-						<div class="col-md-8">
-							<h2>Você Pode Mudar A Sua Realidade <br>Agora Mesmo!</h2>
-							<br>
-							<p>Se você realmente quer perder peso, Phenatrim definitivamente é a melhor solução que você pode encontrar hoje no mercado. Este emagrecedor é fruto de uma longa investigação que finalmente resultou no desenvolvimento de uma perfeita formulação que age sinergicamente no organismo para promover o equilíbrio da função metabólica e contribuir consideravelmente para o processo de emagrecimento.</p>
-							<p>E para obter melhores resultados tome Phenatrim corretamente, mantenha uma alimentação saudável e pratique atividades físicas de forma moderada e sempre acompanhada por um profissional qualificado.</p>
-							<hr>
-
-							<div id="icones">
-								<span>
-									<i class="flaticon-drugs"></i>
-									Sem necessidade de receita
-								</span>
-								<span>
-									<i class="flaticon-mortar"></i>
-									Sem substâncias farmacêuticas perigosas
-								</span>
-								<span>
-									<i class="flaticon-charity"></i>
-									Sem efeitos colaterais nocivos
-								</span>
-							</div>
-						</div>
-						<div class="col-md-4"></div>
-						<div class="col-md-12 visible-sm visible-xs">
-							<img class="img-responsive" src="assets/img/img-doctor.png" alt="">
-						</div>
-					</div>
-				</div>
-			</section>
 
 			<section id="na-midia">
 				<div class="container">
