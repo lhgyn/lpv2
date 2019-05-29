1a4ca46a222f75e1279058144781b7cdde853155
Peterson Macedo <petersondmb@hotmail.com>
2019-05-29 11:05:39 -0300

    adicionando bloco 2 na página ads2

--- views/ads2.php
+++ views/ads2.php
@@ -59,3 +59,18 @@
 			</div>
 		</section>  	
 	</div>
+
+	<section id="section-two">
+		<div class="container">
+			<div class="col-md-8">
+				<h2>Absolutamente Tudo O Que Você Precisa Para Conquistar A Melhor Versão De Si Mesma!</h2>
+				<br>
+				<p>Phenatrim é uma fórmula emagrecedora 100% natural, composta por uma mistura única de ingredientes cuidadosamente selecionados e que oferecem benefícios comprovados cientificamente para perda de peso e queima de gordura.</p>
+
+				<p class="text-center"><img id="stamp-guarantee" src="assets/img/selos.png" alt=""></p>
+			</div>
+			<div class="col-md-4">
+				<figure id="figure-card"><img class="img-responsive" src="./assets/img/resultado-60d.jpg" alt=""></figure>
+			</div>
+		</div>
+	</section>
