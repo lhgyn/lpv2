f9d906ccf912656de56f1541fde5d69ac34ab23c
Peterson Macedo <petersondmb@hotmail.com>
2019-07-05 10:13:34 -0300

    alterando o texto da página sobre nós

--- views/sobre-nos.php
+++ views/sobre-nos.php
@@ -5,12 +5,33 @@
 			<hr>
 		</div>
 		<div class="col-md-12" style="color: #333">
-			<p>Phenatrim foi desenvolvido para suprir especialmente as necessidades de cada mulher que precisa perder peso e queimar gordura de forma segura e eficaz.</p>
-			<p>O nosso principal objetivo é contribuir significativamente para você obter resultados reais e duradouros.</p>
-			<p>Por isso, assumimos o compromisso de trabalhar apenas com as melhores substâncias, na dosagem ideal e totalmente de acordo com as normas vigentes da ANVISA, oferecendo ao mercado brasileiro um produto de alta qualidade.</p>
-			<p>Absolutamente tudo o que fazemos é pensando em proporcionar mais qualidade de vida a cada mulher.</p>
-			<p>Mais do que transações comerciais, desejamos estabelecer com você um relacionamento onde a busca diária por mais saúde, bem-estar e melhores formas de cuidar do próprio corpo sejam objetivos comuns.</p>
-			<p>Dessa forma, nós da Phenatrim convidamos você a encontrar a melhor versão de si mesma e sentir-se mais confiante e feliz.</p>
+			<p>Phenatrim é um produto que pertence a MedRx.</p>
+			<p>A MedRx desenvolve nutracêuticos para saúde. O nosso objetivo é educar, inspirar e motivar as pessoas a terem mais saúde e qualidade de vida.</p>
+			<p>Por isso, dedicamos o nosso trabalho de cada dia a desenvolver e disponibilizar no mercado produtos de alta qualidade, com fórmulas altamente potentes, eficazes e seguras, com base na poderosa interação da ciência e na pureza de cada composto clinicamente estudado para contribuir significativamente com a otimização e o equilíbrio da saúde de cada um de nossos clientes.</p>
+			<p>Diante disso, nossa missão é simples: ajudar as pessoas a viverem mais e proporcionar a cada uma delas mais saúde e bem-estar.</p>
+			<p>Enfim, estamos verdadeiramente dedicados a ajudar você consumidor a potencializar os seus resultados e transformar a sua vida através de soluções inovadoras e eficientes.</p>
+		</div>
+		<div class="col-md-12 title">
+			<h3>Compromisso com a excelência</h3>
+		</div>
+		<div class="col-md-12" style="color: #333">
+			<p>Entendemos que o desenvolvimento de fórmulas clinicamente pesquisadas, compostas por ingredientes chaves e aprovados pela ANVISA proporcionam uma qualidade superior e favorece melhores resultados. Além disso, permite a criação de produtos altamente seguros para a sua saúde e para a saúde da sua família.</p>
+			<p>Sendo assim, a integridade do nosso trabalho e a nossa crença na importância de ajudar as pessoas a serem mais saudáveis, inspiram o nosso compromisso com a excelência todos os dias na elaboração dos melhores nutracêuticos para a saúde possíveis.</p>
+			<p>Não perca mais tempo e conheça a melhor versão de si mesma!</p>
+		</div>
+		<div class="col-md-12 title">
+			<h3>Verdade Na Fabricação</h3>
+		</div>
+		<div class="col-md-12" style="color: #333">
+			<p>Desde o primeiro momento que começamos a desenvolver qualquer produto da MedRx, selecionamos apenas ingredientes de alta qualidade e apoiados pela ciência para garantir a segurança, a pureza e a eficácia de nossos produtos.</p>
+			<p>Além disso, todos os nossos parceiros de fabricação cumprem rigorosamente todas as normas vigentes da ANVISA e atendem todos os padrões de qualidade.</p>
+		</div>
+		<div class="col-md-12 title">
+			<h3>Potência é Chave Para Acelerar os Resultados</h3>
+		</div>
+		<div class="col-md-12" style="color: #333">
+			<p>Nós acreditamos que fórmulas potentes e projetadas clinicamente com as melhores matérias-primas disponíveis é o segredo para otimizar os seus resultados.</p>
+			<p>Então, confiamos piamente em nossos produtos e garantimos a sua satisfação. Afinal de contas, não queremos apenas transações comerciais, mas queremos ser dignos da sua confiança e contribuir verdadeiramente para a sua saúde.</p>
 		</div>
 	</div>
 </section>
